86ad2d2bde0367e8fbb07a855c0899e8ccb1762a
Logan Yang <logancyang@gmail.com>
2020-08-10 18:44:55 -0400

    Add test for datetime util

--- tests/test_weather_competition.py
+++ tests/test_weather_competition.py
@@ -1,5 +1,13 @@
-from weather_competition import __version__
+from datetime import datetime
+from weather_competition import __version__, app
 
 
 def test_version():
     assert __version__ == '0.1.0'
+
+
+def test__get_datetime():
+    dt = datetime(2020, 8, 10, 18, 30, 44, 493161)
+    assert app._get_datetime(0, ref_dt=dt) == 1597017600
+    assert app._get_datetime(1, ref_dt=dt) == 1596931200
+    assert app._get_datetime(2, ref_dt=dt) == 1596844800
